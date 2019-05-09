c4bbd41b7e3113f57cdb5492dc94e471aa49deeb
shiyuan <shiyuan_wang@optum.com>
2019-05-09 15:15:52 -0400

    update forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,18 +1,19 @@
 const request = require('request')
 
-const forecast=(latitude, longitude, callback)=>{
-    const url = 'https://api.darksky.net/forecast/92674a5322014daed51e61740e1fe22d/'+latitude+','+longitude
-//url is shorthand way to write
-    request({url, json:true},(error, {body})=>{
-        if(error){
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/92674a5322014daed51e61740e1fe22d/' + latitude + ',' + longitude
+    //url is shorthand way to write
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
             callback('Server not connected.', undefined)
-        }else if(body.error){
+        } else if (body.error) {
             callback('location not found', undefined)
-        }else{
-            callback(undefined, {
-                temperature:body.currently.temperature,
-                precip:body.currently.precipProbability
-            })
+        } else {
+            callback(undefined,
+                body.daily.data[0].summary + ' It is currently ' +
+                body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + '. There is a ' + body.currently.precipProbability +'% chance of rain.'
+
+            )
         }
     })
 }
